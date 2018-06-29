6a3da50bedef44dda1aae83cc89af99942a06d52
FiaFia <xiaobai080707@163.com>
2018-06-29 11:25:01 +0800

    Update salaryCalculator.py

--- salaryCalculator.py
+++ salaryCalculator.py
@@ -1,3 +1,11 @@
+# coding=utf-8
+"""
+@author:FiaFia
+@data:2018/6/16
+@version:Python3.6
+"""
+
+
 def taxRate(base):
     if base < 0:
         tax = 0
@@ -16,52 +24,57 @@ def taxRate(base):
     elif base > 80000:
         tax = base * 0.45 - 13505
 
-    print('Tax of salary is : %d' % tax)
+    #print('Tax of salary is : %d' % tax)
 
     return tax
 
-def salaryAfterTax(salary_before_tax):
 
-    #免征点3500 ,个人五险一金比率，养老8%，医疗2%，失业0.2%，公积金12%
-    threshold = 3500
-    old_age_rating = 0.08
-    medical_rating = 0.02
-    unemployment_rating = 0.002
-    housing_fund_rating = 0.12
+def salaryAfterTax(salaryBeforeTax):
+
+    #旧个税免征点3500，新个税免征点5000,个人五险一金比率，养老8%，医疗2%，失业0.2%，公积金12%
+    oldThreshold = 3500
+    newThreshold = 5000
+    oldAgeRating = 0.08
+    medicalRating = 0.02
+    unemployRating = 0.002
+    housingFundRating = 0.12
 
     # 2016年社平工资7706，五险一金上限是社评三倍工资
-    average_salary = 7706
-    triple_average_salary = 3 * average_salary
+    averageSalary = 7706   # 2016社评工资7706， 2017社评工资8467
+    tripleAverageSalary = 3 * averageSalary
 
-    if salary_before_tax < triple_average_salary:
-        total_insurance = salary_before_tax * (old_age_rating + medical_rating + unemployment_rating + housing_fund_rating)
-        housing_fund = salary_before_tax * housing_fund_rating
+    if salaryBeforeTax < tripleAverageSalary:
+        totalInsurance = salaryBeforeTax * (oldAgeRating + medicalRating + unemployRating + housingFundRating)
+        housingFund = salaryBeforeTax * housingFundRating
     else:
-        total_insurance = triple_average_salary * (
-            old_age_rating + medical_rating + unemployment_rating + housing_fund_rating)
-        housing_fund = triple_average_salary * housing_fund_rating   #公积金封顶
-        #housing_fund = salary_before_tax * housing_fund_rating     #公司给补超额公积金部分
+        totalInsurance = tripleAverageSalary * (
+            oldAgeRating + medicalRating + unemployRating + housingFundRating)
+        housingFund = tripleAverageSalary * housingFundRating   #公积金封顶
+        #housingFund = salaryBeforeTax * housingFundRating     #如果公司给补超额公积金部分，则用这个计算
 
     # 纳税额
-    payment = salary_before_tax - total_insurance - threshold
-    tax = taxRate(payment)
+    oldPayment = salaryBeforeTax - totalInsurance - oldThreshold
+    oldTax = taxRate(oldPayment)
+    newPayment = salaryBeforeTax - totalInsurance - newThreshold
+    newTax = taxRate(newPayment)
 
     # 税后工资
-    salary_after_tax = salary_before_tax - total_insurance - tax
-    actual_income = salary_after_tax + housing_fund * 2
+    oldSalaryAfterTax = salaryBeforeTax - totalInsurance - oldTax
+    oldActualIncome = oldSalaryAfterTax + housingFund * 2
+    newSalaryAfterTax = salaryBeforeTax - totalInsurance - newTax
+    newActualIncome = newSalaryAfterTax + housingFund * 2
+    Gap = newActualIncome - oldActualIncome
 
-    print('Housing Fund is : %d' % housing_fund)
-    print('Total insurance is : %d ' % total_insurance)
-    print('Tax of salary is : %d' % tax)
-    print('Salary after tax is : %d' % salary_after_tax)
-    print('Actual income including housing fund is : %d ' % actual_income)
-    print('Actual income Percent is : %.2f %%' % float(actual_income*100/salary_before_tax))
+    print('Total insurance is %d and Housing fund is %d' % (totalInsurance, housingFund))
+    print('New tax is %d and Previous tax is %d, could save more %d' % (newTax, oldTax, Gap))
+    print('New net income is %d and Previous net income is %d' % (newSalaryAfterTax, oldSalaryAfterTax))
+    print('New income with housing fund is %d and Previous income with housing fund is %d ' % (newActualIncome, oldActualIncome))
+    print('New Actual income Percent is : %.2f %%' % float(newActualIncome * 100 / salaryBeforeTax))
+    print('Previous Actual income Percent is : %.2f %%' % float(oldActualIncome * 100 / salaryBeforeTax))
 
-    return salary_after_tax
+    return salaryAfterTax
+
 
 if __name__ == '__main__':
     salary = int(input('Please input your Salary before tax:'))
     salaryAfterTax(salary)
-
-
-
